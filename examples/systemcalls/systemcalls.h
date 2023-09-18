529de3d3bb59a7589919928218c4f42d9a4a52d7
Nicholas-Buckley77 <nibu7565@colorado.edu>
2023-09-17 23:10:12 -0600

    Unit tests passing with additions to syscalls.c and h with full error logging! Thank goodness...........

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#define _XOPEN_SOURCE
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <syslog.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
 
 bool do_system(const char *command);
 
