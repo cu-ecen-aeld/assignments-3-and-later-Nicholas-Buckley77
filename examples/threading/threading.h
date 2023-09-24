d28272c37c68a9c599d1418d2f9f5d2f1f184d95
Nicholas-Buckley77 <nibu7565@colorado.edu>
2023-09-24 15:10:34 -0600

    adding implementation of the threading functions working for part 1

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -8,12 +8,11 @@
  * the joiner thread.
  */
 struct thread_data{
-    /*
-     * TODO: add other values your thread will need to manage
-     * into this structure, use this structure to communicate
-     * between the start_thread_obtaining_mutex function and
-     * your thread implementation.
-     */
+    
+     pthread_t* id;
+     pthread_mutex_t* mutex;
+     int waitToObtainMs;
+     int waitToReleaseMs;
 
     /**
      * Set to true if the thread completed with success, false
