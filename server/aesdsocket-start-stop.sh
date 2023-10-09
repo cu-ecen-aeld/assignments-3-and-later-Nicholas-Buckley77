fc07d33c330b20f664aad0e05e0f2da203cd3bdf
Nicholas-Buckley77 <nibu7565@colorado.edu>
2023-10-08 19:59:11 -0600

    Added start stop script for daemon start and stop of socket program

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#Assignment 5 AESD Nicholas Buckley start stop script from lecture linux system init
+
+#Check argument count...
+if [ $# -ne 1 ]
+then #if it's less than required exit error 1 and print
+
+    echo "$# is not enough arguments."
+    echo " please use start or stop arguments"
+    exit 1
+    
+fi
+
+case "$1" in
+        start)
+            echo "Starting aesdsocket"
+            start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket
+            ;;
+        stop)
+            echo "Stopping aesdsocket"
+            start-stop-daemon -K -n aesdsocket
+            ;;
+
+        *)
+            echo "Usage: $0 {start|stop}"
+            exit 1
+
+esac
+
+exit 0
